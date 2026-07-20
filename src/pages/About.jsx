9df93f5886c65288640fde1c74f70245e25db447
JosephMelesse <josephmelesse1@gmail.com>
2026-07-19 19:21:49 -0700

    About/About.jsx: render story from data

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,13 @@
 // ABOUT: Luis
-function About() {
-  return <section>{/* TODO: restaurant story */}</section>;
+function About({ t }) {
+  return (
+    <section className="about">
+      <div className="container">
+        <h1>{t?.about?.title}</h1>
+        <p>{t?.about?.story}</p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
